cf27d8f52c53cb61d9f09f48ee17bbb8edcc7584
henricgustafsson <50734521+henricgustafsson@users.noreply.github.com>
2019-05-20 16:53:58 +0200

    commented and refactored

--- Week 1/calculator/calculator/src/main/java/lexicon/se/calculator/calculator/ScannerInteraction.java
+++ Week 1/calculator/calculator/src/main/java/lexicon/se/calculator/calculator/ScannerInteraction.java
@@ -1,5 +1,6 @@
 package lexicon.se.calculator.calculator;
 
+
 import java.util.Scanner;
 
 public class ScannerInteraction {
@@ -11,30 +12,70 @@ public class ScannerInteraction {
 		this.scanner = scanner;
 	}
 	
-	public int[] getNumbersToCalculateArr() {
-		int numbersToCalculate = getNumber("How many numbers do you want to calculate");
-							
-		int[] numbersToCalculateArr = new int[numbersToCalculate];
-		
-		for (int i = 0; i < numbersToCalculateArr.length; i++) {
-			numbersToCalculateArr[i] = getNumber("Enter number");
+	
+	/**Promts user for number, displays
+	 * error message if incorrect input & recurses
+	 * @return int number
+	 */
+	public int getNextInt() {
+		System.out.println("Enter Number");
+		int number =0;
+		try {
+			number = scanner.nextInt();
+			//Clear the number
+			scanner.nextLine();
 		}
+		catch(Exception e){
+			System.out.println("Please enter a number");
+			getNextInt();
+		}
+		
+		return number;
+	}
+	
+	/**Prompts user for numbers, at least 2 required
+	 * validates using checkIfSmallerThan().
+	 * @return int[] numbersToCalculateArr
+	 */
+	public int[] getNumbersToCalculateArr() {
+		System.out.println("How many numbers do you want to calculate");
+		int numbersToCalculate = getNextInt();
+		
+		/*validate using numbersToCalculateArr, if user inputs at least 2 numbers 
+		 * new array with n elements created, otherwise an empty array is created.
+		 */
+			int[] numbersToCalculateArr= checkIfSmallerThan(numbersToCalculate, 2,
+					"Please enter atleast 2 numbers") ? new int[numbersToCalculate] : new int[0];
+			
+			if(numbersToCalculateArr.length >0) {
+				for (int i = 0; i < numbersToCalculateArr.length; i++) {
+					numbersToCalculateArr[i] = getNextInt();
+				}
+			}
+			else {
+				throw new IllegalArgumentException();
+			}
+			
+		
+		
 		return numbersToCalculateArr;
 	}
 	
 	
+	/**Print menu options, takes input using scanner
+	 * using getNextInt()
+	 * @return int number
+	 */
 	public int menuHandler() {
 		System.out.println("\nUse the keyboard to choose option\n*************\n1.Addition\n2.Subtraction\n3.Multiplication\n4.Division");
-		int number = scanner.nextInt();
-		scanner.nextLine();
+		int number =getNextInt();
 		return number;
 	}
-	public int getNextInt() {
-		System.out.println("Enter Number");
-		int number = scanner.nextInt();
-		scanner.nextLine();
-		return number;
-	}
+	
+	
+	/**Prompts user, if user enters "y" returns true, else false
+	 * @return boolean
+	 */
 	public boolean askIfContinue() {
     	System.out.println("Continue? (y/n)");
     	String input = scanner.nextLine();      	
@@ -42,22 +83,16 @@ public class ScannerInteraction {
     	
     }
 	
-	public int getNumber(String message) {
-    	System.out.println(message);
-    	int number =0;
-    	try {
-    		number = scanner.nextInt();
-    		scanner.nextLine();
-    	}
-    	catch(Exception e) {
-			System.out.println("Please enter a number!");
-			scanner.nextLine();
-			getNumber(message);
-		}
-    	return number;
-    }
+	
     
-    public boolean CheckifSmallerThan(int numberToCheck,int minimumNumber, String message) {
+    /** Checks if parameter numberToCheck is smaller than parameter
+     * minimumNumber, displays message
+     * @param numberToCheck
+     * @param minimumNumber
+     * @param message
+     * @return
+     */
+    public boolean checkIfSmallerThan(int numberToCheck,int minimumNumber, String message) {
     	if(numberToCheck < minimumNumber) {
     		System.out.println(message);
     		scanner.nextLine();
